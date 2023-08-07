8ce64275b1b2542ee3e463ddf212b96a1784fde2
Liveral <101503612+Liveral@users.noreply.github.com>
2023-08-07 13:29:16 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 560 ms, Memory: 56480 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,27 +1,36 @@
 
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
 		StringBuilder sb=new StringBuilder();
-		int N=sc.nextInt();
-		int M=sc.nextInt();
+		
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
 		
 		int[] arr=new int[N+1];
 		
+		st=new StringTokenizer(br.readLine());
 		for(int i=1;i<=N;i++) {
-			int n=sc.nextInt();
+			int n=Integer.parseInt(st.nextToken());
 			arr[i]=arr[i-1]+n;
 		}
 		
 		
 		for(int i=0;i<M;i++) {
-			int a=sc.nextInt();
-			int b=sc.nextInt();
+			st=new StringTokenizer(br.readLine());
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
 			
 			sb.append(arr[b]-arr[a-1]);
 			sb.append("\n");
