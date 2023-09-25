cb05b63dac4ef6e60978ed1765139f7cc9e006fe
Liveral <101503612+Liveral@users.noreply.github.com>
2023-09-25 12:07:50 +0900

    [Gold IV] Title: 연구소, Time: 776 ms, Memory: 298848 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,117 +2,116 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-
-	public static int[][] arr;
-    
-    public static int[] dx= {0,1,-1,0};
-    public static int[] dy= {1,0,0,-1};
-    
-    
-    public static int N,M;
-    public static int max=0;
-    
-    
-    public static void main(String[] arg)  {
-        Scanner sc=new Scanner(System.in);
-        
-        N=sc.nextInt();
-        M=sc.nextInt();
-        arr=new int[N][M];
-        
-       for(int i=0;i<N;i++) {
-    	   for(int j=0;j<M;j++) {
-    		   arr[i][j]=sc.nextInt();
-    	   }
-       }
-    
-        
-       dfs(0);
-       
-       System.out.println(max);
-        
-    }
-    
-    public static void dfs(int wallcnt) {
-        if(wallcnt==3) {
-     	   bfs();
-     	   return;
-        }
-        for(int i=0;i<N;i++) {
-     	   for(int j=0;j<M;j++) {
-     		   if(arr[i][j] == 0) {
-                     arr[i][j] = 1;
-                     dfs(wallcnt+1);
-                     arr[i][j] = 0;
-                 }
-     	   }
-        }
-     }
-
-    static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-
-    	for(int i=0;i<N;i++) {
-    		for(int j=0;j<M;j++) {
-    			if(arr[i][j]==2) {
-    				q.add(new Node(i,j));
-    			}
-    		}
-    	}
-        //원본 연구소를 바꾸지 않기 위한 카피맵 사용
-    	int copy_arr[][]=new int[N][M];
-    	for(int i=0;i<N;i++) {
-    		copy_arr[i]=arr[i].clone();
-    }
-    	while(!q.isEmpty()) {
-    		Node tmp=q.poll();
-    		int x=tmp.x;
-    		int y=tmp.y;
-    		for(int i=0;i<4;i++) {
-    			int nx=x+dx[i];
-    			int ny=y+dy[i];
-    			
-    			if(nx<0||ny<0||nx>=N||ny>=M) continue;
-    			
-    			if(copy_arr[nx][ny]==0) {
-    				copy_arr[nx][ny]=2;
-    				q.add(new Node(nx,ny));
-    			}
-    		}
-    		int num=0;
-    		for(int i=0;i<N;i++) {
-    			for(int j=0;j<M;j++) {
-    				if(copy_arr[i][j]==0) num++;
-    			}
-    		}
-    		//max=Math.max(num, max);
-    	}
-        //SafeZone 확인
-        funcSafeZone(copy_arr);
-    }
-
-    private static void funcSafeZone(int[][] copyMap) {
-        int safeZone =0;
-        for(int i=0; i<N ; i++) {
-            for(int j=0; j<M; j++) {
-                if(copyMap[i][j] == 0) {
-                    safeZone++;
-                }
-            }
-        }
-        if (max < safeZone) {
-            max = safeZone;
-        }
-    }
-
-    //Queue에 좌표값 x,y를 넣기 위함.
-    static class Node {
-        int x;
-        int y;
-        Node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+	
+	public static boolean[] visited=new boolean[26];
+	public static int[][] map;
+	public static int[][] copy_map;
+	
+	public static int max=0;
+	public static int M,N;
+	public static int[] dx= {-1,0,0,1};
+	public static int[] dy= {0,1,-1,0};
+	
+	static class Node{
+		int x;
+		int y;
+		Node(int x, int y){
+			this.x=x;
+			this.y=y;
+		}
+	}
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+	   //Scanner sc=new Scanner(System.in);
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		
+		map=new int[N][M];
+		copy_map=new int[N][M];
+		
+		
+		
+		
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
+			}
+			
+		}
+		
+		
+		
+		dfs(0);
+		
+		System.out.print(max);
+		
+		
+	}
+	public static void bfs() {
+		Queue<Node> q=new LinkedList<>();
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				if(map[i][j]==2) {
+					q.add(new Node(i,j));
+				}
+			}
+		}
+		
+		  for (int i = 0; i < N; i++) {
+	            copy_map[i] = map[i].clone();
+	        }
+		
+		while(!q.isEmpty()) {
+			Node now=q.poll();
+			int x=now.x;
+			int y=now.y;
+			
+			for(int k=0;k<4;k++) {
+				int cx=x+dx[k];
+				int cy=y+dy[k];
+				
+				if(cx<0||cy<0||cx>=N||cy>=M) continue;
+				
+				if(copy_map[cx][cy]==0) {
+					copy_map[cx][cy]=2;
+					q.add(new Node(cx,cy));
+				}
+				
+			}
+		}
+		checkzero(copy_map);
+	}
+	public static void checkzero(int[][] copy_map) {
+		int ans=0;
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				if(copy_map[i][j]==0) ans++;
+			}
+		}
+		max=Math.max(max, ans);
+	}
+	
+	public static void dfs(int depth) {
+		if(depth==3) {
+			bfs();
+			return;
+		}
+		else {
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<M;j++) {
+					if(map[i][j]==0) {
+						map[i][j]=1;
+						dfs(depth+1);
+						map[i][j]=0;
+					}
+				}
+			}
+		}
+	}
+	
 }
-
