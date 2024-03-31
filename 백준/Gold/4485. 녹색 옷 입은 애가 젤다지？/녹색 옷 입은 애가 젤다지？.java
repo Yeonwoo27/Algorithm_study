9169ad45659eec755b9ea5fb48cd0f996c290692
Yeonwoo27 <161433883+Yeonwoo27@users.noreply.github.com>
2024-03-31 21:59:22 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 224 ms, Memory: 18952 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,19 +1,17 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
-	public static int N;
 	public static int[][] arr;
-	public static int[][] dist;
-	public static int[] dx= {-1,0,1,0};
-	public static int[] dy= {0,1,0,-1};
+	public static boolean[][] visited;
+	public static int[] dx= {0,0,1,-1};
+	public static int[] dy= {1,-1,0,0};
+	public static PriorityQueue<Node> pq=new PriorityQueue<>();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,11 +22,12 @@ public class Main{
 		while(true) {
 			
 			st = new StringTokenizer(br.readLine());
-			N=Integer.parseInt(st.nextToken());
+			int N=Integer.parseInt(st.nextToken());
 			if(N==0) break;
 			
-			
 			arr=new int[N][N];
+			visited=new boolean[N][N];
+			int min=Integer.MAX_VALUE;
 			for(int i=0;i<N;i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j=0;j<N;j++) {
@@ -36,53 +35,41 @@ public class Main{
 				}
 			}
 			
-			dijkstra();
+			pq.add(new Node(0,0,arr[0][0]));
 			
+			while(!pq.isEmpty()) {
+				Node current=pq.poll();
+				int x=current.x;
+				int y=current.y;
+				int weight=current.weight;
+				
+				
+				for(int i=0;i<4;i++) {
+					int cx=x+dx[i];
+					int cy=y+dy[i];
+					
+					if(cx==N-1&&cy==N-1) {
+						min=Math.min(min, weight+arr[N-1][N-1]);
+					}
+					
+					if(cx<0||cy<0||cx>=N||cy>=N) continue;
+					if(visited[cx][cy]) continue;
+				
+					visited[cx][cy]=true;
+					pq.add(new Node(cx,cy,weight+arr[cx][cy]));
+					
+				}
+				
+			}
 			sb.append("Problem ");
 			sb.append(num);
 			sb.append(": ");
-			sb.append(dist[N-1][N-1]);
+			sb.append(min);
 			sb.append("\n");
 			num++;
 		}
 		
 		System.out.println(sb);
-	}
-	public static void dijkstra() {
-		dist=new int[N][N];
-		boolean[][] visited= new boolean[N][N];
-		
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		for(int i=0; i<N; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-		dist[0][0] = arr[0][0];	
-		pq.offer(new Node(0, 0, arr[0][0]));
-		
-		while(!pq.isEmpty()) {
-			Node current=pq.poll();
-			int x=current.x;
-			int y=current.y;
-			int weight=current.weight;
-			
-			if(visited[x][y]) continue;
-			
-			visited[x][y]=true;
-			if(x==N-1&&y==N-1) return;
-			
-			for(int i=0;i<4;i++) {
-				int cx=x+dx[i];
-				int cy=y+dy[i];
-				
-				if(cx<0||cy<0||cx>=N||cy>=N) continue;
-				
-				if(!visited[cx][cy]&&dist[cx][cy]>dist[x][y]+arr[cx][cy]) {
-					dist[cx][cy]=dist[x][y]+arr[cx][cy];
-					pq.add(new Node(cx,cy,dist[cx][cy]));
-				}
-					
-				
-			}
-			
-		}
 	}
 	public static class Node implements Comparable<Node>{
 		int x;
